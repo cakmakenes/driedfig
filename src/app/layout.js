7a67b3b11a0eb4646d7ea5ef3ceda8bafeb111f7
cakmakenes <enes.cakmak@ug.bilkent.edu.tr>
2025-10-15 01:03:52 +0300

    livepreview deleted for sanity(no need)

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,7 +2,6 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
-import { VisualEditing } from "next-sanity/visual-editing";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,7 +27,6 @@ export default function RootLayout({ children }) {
         <Header />
         {children}
         <Footer />
-        <VisualEditing />
       </body>
     </html>
   );
